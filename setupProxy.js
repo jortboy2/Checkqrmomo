c1c01cee573bbaa5894f645ef8f7c56ee970d549
jortboy2 <vonghung849@gmail.com>
2024-10-18 14:09:30 +0700

    fix

--- setupProxy.js
+++ setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (app) => {
   app.use(
     createProxyMiddleware({
-      target: "https://business.momo.vn",
+      target: "https://business.momo.vn/",
       changeOrigin: true,
     })
   );
